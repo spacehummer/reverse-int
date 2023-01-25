9c313137c46d982cd1d4162b26be78581fbcf170
Yuriy Lavrushin <yuriy.lavroushin@gmail.com>
2023-01-25 23:52:59 +0400

    feat: implement function for revers integer. Pass all local tests.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 module.exports = function reverse (n) {
-  
+  const digitsArr = n.toString(10).split('').reverse();
+  return digitsArr[digitsArr.length - 1] === '-' ?Number(digitsArr.join('').slice(0, -1)) : Number(digitsArr.join(''));
 }
